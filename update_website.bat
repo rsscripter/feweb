2cccd212f9d6e29313b4cdf0b15b7eeaa0296fb2
Muhammad Gill <mugill@blackberry.com>
2018-06-08 16:41:08 -0400

    'pop'

--- update_website.bat
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "pops"
-git push origin master
---
+++ update_website.py
@@ -0,0 +1,150 @@
+import os
+import sys
+
+img_extensions = [
+	"ase",
+	"art",
+	"bmp",
+	"blp",
+	"cd5",
+	"cit",
+	"cpt",
+	"cr2",
+	"cut",
+	"dds",
+	"dib",
+	"djvu",
+	"egt",
+	"exif",
+	"gif",
+	"gpl",
+	"grf",
+	"icns",
+	"ico",
+	"iff",
+	"jng",
+	"jpeg",
+	"jpg",
+	"jfif",
+	"jp2",
+	"jps",
+	"lbm",
+	"max",
+	"miff",
+	"mng",
+	"msp",
+	"nitf",
+	"ota",
+	"pbm",
+	"pc1",
+	"pc2",
+	"pc3",
+	"pcf",
+	"pcx",
+	"pdn",
+	"pgm",
+	"PI1",
+	"PI2",
+	"PI3",
+	"pict",
+	"pct",
+	"pnm",
+	"pns",
+	"ppm",
+	"psb",
+	"psd",
+	"pdd",
+	"psp",
+	"px",
+	"pxm",
+	"pxr",
+	"qfx",
+	"raw",
+	"rle",
+	"sct",
+	"sgi",
+	"rgb",
+	"int",
+	"bw",
+	"tga",
+	"tiff",
+	"tif",
+	"vtf",
+	"xbm",
+	"xcf",
+	"xpm",
+	"3dv",
+	"amf",
+	"ai",
+	"awg",
+	"cgm",
+	"cdr",
+	"cmx",
+	"dxf",
+	"e2d",
+	"egt",
+	"eps",
+	"fs",
+	"gbr",
+	"odg",
+	"svg",
+	"stl",
+	"vrml",
+	"x3d",
+	"sxd",
+	"v2d",
+	"vnd",
+	"wmf",
+	"emf",
+	"art",
+	"xar",
+	"png",
+	"webp",
+	"jxr",
+	"hdp",
+	"wdp",
+	"cur",
+	"ecw",
+	"iff",
+	"lbm",
+	"liff",
+	"nrrd",
+	"pam",
+	"pcx",
+	"pgf",
+	"sgi",
+	"rgb",
+	"rgba",
+	"bw",
+	"int",
+	"inta",
+	"sid",
+	"ras",
+	"sun",
+	"tga"
+]
+
+imgs = []
+descs = []
+
+files = [f for f in os.listdir('.') if os.path.isfile(f)]
+for f in files:
+    filename, file_extension = os.path.splitext(f)
+    if file_extension[1:] in img_extensions:
+        imgs.append(f)
+        descs.append(filename + ".txt")
+
+img_file = open("images.gill", "w")
+img_file.write('\n'.join(imgs))
+img_file.close()
+
+d_file = open("descriptions.gill", "w")
+d_file.write('\n'.join(descs))
+d_file.close()
+
+os.system("git add .")
+os.system("git commit -m 'pop'")
+os.system('git push origin master')
+
+sys.exit()
+
